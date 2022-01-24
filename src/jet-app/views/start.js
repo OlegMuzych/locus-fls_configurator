bdc9c1c6fd5d2add52daad1eaecdcd6b51ad858a
muz_ol <olegMuzych@yandex.ru>
2022-01-24 13:29:08 +0300

    init electron

---
+++ src/jet-app/views/start.js
@@ -0,0 +1,3 @@
+export default {
+	template:"Start page2333", css:"webix_shadow_medium app_start"
+};
---
+++ webpack.renderer.config.js
@@ -0,0 +1,54 @@
+const rules = require('./webpack.rules');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require('webpack');
+let pack = require("./package.json");
+
+rules.push({
+  test: /\.css$/,
+  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+});
+
+rules.push({
+  test: /\.m?js$/,
+  exclude: /(node_modules|bower_components)/,
+  use: {
+    loader: 'babel-loader',
+    options: {
+      "presets": [
+        ["@babel/preset-env", {
+          // "useBuiltIns": "entry"
+        }]
+      ]
+    }
+  }
+});
+
+module.exports = {
+  // Put your normal webpack config below here
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css"
+    }),
+    new webpack.DefinePlugin({
+      VERSION: `"${pack.version}"`,
+      APPNAME: `"${pack.name}"`,
+      PRODUCTION: false,//production,
+      BUILD_AS_MODULE: false,//(asmodule || standalone)
+    })
+  ],
+
+  module: {
+    rules,
+  },
+
+  resolve: {
+    extensions: [".js"],
+    modules: ["./src/jet-app", "node_modules"],
+    alias: {
+      "jet-views": path.resolve(__dirname, "src/jet-app/views"),
+      "jet-locales": path.resolve(__dirname, "src/jet-app/locales")
+    },
+    fallback: { "util": false },
+  },
+};

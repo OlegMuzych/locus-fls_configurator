9dcd0c833c80a9d9fa00d3b7c6fac47354fde159
muz_ol <OlegMuzych@yandex.ru>
2022-03-06 23:06:41 +0300

    fix window ResestLls

--- src/jet-app/views/engineeringmenu/rightmenu/servicemenu.js
+++ src/jet-app/views/engineeringmenu/rightmenu/servicemenu.js
@@ -1,29 +1,78 @@
 import {JetView} from "webix-jet";
+import ResetLlsWindow from "../windows/reset-lls";
+import llsModel from "../../../models/lls-model";
 
 export default class ServiceMenu extends JetView {
     config() {
-        let right_menu_button={
+        let right_menu_button = {
             height: 95,
-            rows:[
+            rows: [
                 {
                     //Кнопки сверху в правом меню-----------------------//
                     paddingY: 20,
                     cols: [
+                        {},
                         {
-
+                            view: "button",
+                            width: 250,
+                            height: 70,
+                            label: "Сервис",
+                            css: "button_right_menu_top_1",
+                            id: "buttonService"
                         },
-                        {view: "button", width: 250, height: 70, label: "Сервис",  popup: "my_pop_2", css: "button_right_menu_top_1", id:"button_right_menu_top_1"},
                         {
                             width: 10,
                         },
-                        {view: "button", width: 250, height: 70, label: "Пароль", css: "button_right_menu_top_1", id:"button_right_menu_top_2"},
                         {
-
-                        }
+                            view: "button",
+                            width: 250,
+                            height: 70,
+                            label: "Пароль",
+                            css: "button_right_menu_top_1",
+                            id: "button_right_menu_top_2"
+                        },
+                        {}
                     ]
                 }
             ]
         };
         return right_menu_button;
+    }
+
+    init() {
+        // Кнопка Сервис -------------------------------//
+        let service = {
+            view: "popup",
+            id: "popService",
+            css: "service_button",
+            width: 245,
+            height: 400,
+            body: {
+                view: "list",
+                data: [
+                    {view: "button", id: "install", location: "Обновить прошивку", name: ""},
+                    {id: "resetLls", location: "Сброс всех настроек", name: ""},
+                ],
+                id:"list",
+                template: "#name# - #location#",
+                autoheight: true,
+                select: true
+            }
+        };
+
+        this.popService = this.ui(service);
+
+        this.$$('buttonService').attachEvent("onItemClick", (id, e) => {
+            console.log('click');
+            this.popService.show($$(id).getNode());
+        });
+
+        this.resetLlsWindow = this.ui(ResetLlsWindow);
+        $$("list").attachEvent("onItemClick", (id,name, e)=>{
+            console.log("click");
+            if(id == 'resetLls'){
+                this.resetLlsWindow.showWindow();
+            }
+        });
     }
 }
---
+++ src/jet-app/views/engineeringmenu/windows/reset-lls.js
@@ -0,0 +1,77 @@
+import {JetView} from "webix-jet";
+
+export default class ResetLlsWindow extends JetView {
+    config() {
+
+        let body = {
+            view: "window",
+            position: "center",
+            width: 650,
+            height: 400,
+            id: "window_show_4",
+            modal: true,
+            css: "window_show",
+            head: {
+                height: 400,
+                rows: [
+                    {},
+                    {
+                        cols: [
+                            {
+                                width: 50,
+                            },
+                            {
+                                view: "button",
+                                type: "image",
+                                image: "assets/images/Warning.png",
+                                id: "closed_2",
+                                css: "set_password_button_icon",
+                                height: 100,
+                            },
+                            {
+                                view: "label",
+                                label: "<p style='font-size: 20px; position: relative; top: -10px; text-align: center;'>Все настройки датчика будут сброшенны</p>",
+                                css: "language_windows_modal",
+                                id: "language_windows_modal_2",
+                                height: 100,
+                                width: 420,
+                            },
+                            {}
+                        ]
+                    },
+                    {
+                        height: 65,
+                    },
+                    {
+                        cols: [
+                            {},
+                            {view: "button", label: "OK", localId: "buttonOk", css: "set_password_button"},
+                            {},
+                            {view: "button", label: "Отмена", localId: "buttonCancel", css: "set_password_button"},
+                            {}
+                        ]
+                    },
+                    {}
+                ]
+            },
+        }
+
+        return body;
+    }
+
+    init(){
+        this.$$('buttonCancel').attachEvent("onItemClick", (id, e) => {
+            console.log('click');
+            this.getRoot().hide();
+        });
+
+        this.$$('buttonOk').attachEvent("onItemClick", (id, e) => {
+            console.log('click');
+            //todo: command resetLls
+        });
+    }
+
+    showWindow() {
+        this.getRoot().show();
+    }
+}

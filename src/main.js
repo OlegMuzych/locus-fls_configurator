4bf8d0ccabd824bbe93477c12cba7fb7f514e053
muz_ol <OlegMuzych@yandex.ru>
2022-03-23 11:14:11 +0300

    add fix

--- src/main.js
+++ src/main.js
@@ -23,6 +23,8 @@ const createWindow = () => {
     }
   });
 
+  mainWindow.setMenuBarVisibility(false);
+
   // and load the index.html of the app.
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 

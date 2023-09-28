3e388c3d9d34ddc7050b9c8196b4edd0ed0a7694
Zwick <zwick@mail.ru>
2023-09-28 15:06:17 +0300

    what nothing working is show save text

--- src/jet-app/views/engineeringmenu/centralmenu/firstSensor/showSaveSettings.js
+++ src/jet-app/views/engineeringmenu/centralmenu/firstSensor/showSaveSettings.js
@@ -8,12 +8,12 @@ export default class ShowSaveSettings extends JetView {
 			view: "label",
 			label: `<p style="position: relative; top: -20px;">${_("save_settings_text_show")}</p>`,
 			height: 50,
-			css: "show_save_settings_dark",
-			id:"show_save_settings",
+			css: "show_save_settings",
+			id: "show_save_settings",
 		};
 
-		let body ={
-			rows:[
+		let body = {
+			rows: [
 				show_save_settings
 			]
 		};
@@ -24,13 +24,34 @@ export default class ShowSaveSettings extends JetView {
 
 	init() {
 
-		if(configFile.theme == 'light'){
-			// webix.html.removeCss( $$("show_save_settings").getNode(), "show_save_settings_dark");
-			webix.html.addCss( $$("show_save_settings").getNode(), "show_save_settings");
+		this.on(this.app, "app:settings:setToLls", () => {
+			webix.html.removeCss($$("show_save_settings").getNode(), "show_save_settings");
+			webix.html.addCss($$("show_save_settings").getNode(), "show_save_settings_dark_green")
+			$$("show_save_settings").refrash();
+
+
+			webix.message({
+				    text:"<p style='font-size:20px;'>test<p/>",
+				    type:"success",
+				    expire:5000,
+				});
+		});
+
+
+		if (configFile.theme == 'light') {
+			webix.html.addCss($$("show_save_settings").getNode(), "show_save_settings");
+
 		}
-		if(configFile.theme == 'dark'){
-			// webix.html.removeCss( $$("show_save_settings").getNode(), "show_save_settings_dark");
-			webix.html.addCss( $$("show_save_settings").getNode(), "show_save_settings_dark");
+		if (configFile.theme == 'dark') {
+			webix.html.addCss($$("show_save_settings").getNode(), "show_save_settings_dark");
+
 		}
+
+
 	};
 }
+
+
+
+
+

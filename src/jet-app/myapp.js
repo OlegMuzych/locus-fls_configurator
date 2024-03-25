9ca519efeb3db753e053937a88c100e8d510a2a4
muz_ol <OlegMuzych@yandex.ru>
2024-03-26 01:47:57 +0300

    add test log fix

--- src/jet-app/myapp.js
+++ src/jet-app/myapp.js
@@ -36,8 +36,8 @@ export default class MyApp extends JetApp{
 
 if (!BUILD_AS_MODULE){
 	webix.ready(() => {
-		const developLog = window.log_for_develop;
-		developLog.pushData(Object.values({q:1,r:2, t:5}));
+		// const developLog = window.log_for_develop;
+		// developLog.pushData(Object.values({q:1,r:2, t:5}));
 
 		webix.CustomScroll.init();
 		return new MyApp().render();
